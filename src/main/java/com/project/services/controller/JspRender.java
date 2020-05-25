2255e7f9b09d482426e5e4a55fbb5f54c604dd0f
Akshat Ahuja <akshat.ahuja@sap.com>
2020-05-26 00:32:55 +0530

    adding a starter UI

--- src/main/java/com/project/services/controller/JspRender.java
+++ src/main/java/com/project/services/controller/JspRender.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class JspRender {
 
-    @GetMapping("/jsp")
-    public String render(){
+    @GetMapping("/index")
+    public String renderIndex(){
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String renderLogin(){
+        return "login";
+    }
+
+    @GetMapping("/registration")
+    public String renderRegister() {
+        return "registration";
     }
 }
