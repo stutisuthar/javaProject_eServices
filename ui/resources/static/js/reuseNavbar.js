22d96c7ca65333a92876dbb2181ff22391467959
vaibhavhirani <vaibhavhirani@gmail.com>
2020-05-27 13:26:17 +0530

    reusing navbar

---
+++ ui/resources/static/js/reuseNavbar.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#navbar-frame").load("/templates/adminNavbar.html")
+});
+
+
+$(document).ready(function() {
+    $("#navbar-user-frame").load("/templates/navbar.html");
+})
