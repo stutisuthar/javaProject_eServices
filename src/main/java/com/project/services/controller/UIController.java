b5ff7e7b8fdae28d6caff1b9945b20dba8a2d77e
cmaj <15957531+fushizen@users.noreply.github.com>
2020-06-02 23:43:44 +0530

    Soap issue.

--- src/main/java/com/project/services/controller/UIController.java
+++ src/main/java/com/project/services/controller/UIController.java
@@ -13,14 +13,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 // import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 
 // Session Management
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class UIController {
@@ -75,16 +81,30 @@ public class UIController {
         return "landing";
     }
 
-    @GetMapping("/service")
-    public String renderService() {
-        return "service";
-    }
-
     @GetMapping("/navbar")
     public String renderNavbar(Model model, HttpServletRequest request) {
         String userName = (String) request.getSession().getAttribute("userName");
         model.addAttribute("userName", userName);
         return "navbar";
+    }
+
+    // @GetMapping("/services")
+    // public String renderService() {
+    // return "service";
+    // }
+
+    // @GetMapping("/test")
+    // public String renderTest(@RequestParam(name = "name", required = false,
+    // defaultValue = "World") String name,Model model){
+    // model.addAttribute("name", name);
+    // return "test";
+    // }
+
+    @GetMapping("/test")
+    public String renderTest(Model model) {
+        Service user = new Service();
+        model.addAttribute("user", user);
+        return "test";
     }
 
     @PostMapping("/signout")
@@ -93,17 +113,19 @@ public class UIController {
         return "redirect:/login";
     }
 
-    // @GetMapping("/test")
-    // public String renderTest(@RequestParam(name = "name", required = false,
-    // defaultValue = "World") String name,Model model){
-    // model.addAttribute("name", name);
-    // return "test";
-    // }
-    @GetMapping("/test")
-    public String renderTest(Model model) {
-        Service user = new Service();
-        model.addAttribute("user", user);
-        return "test";
+    // @PostMapping(value = "/services/", consumes =
+    // MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    // "application/x-www-form-urlencoded"
+    @PostMapping("/services/")
+    public String renderService(@ModelAttribute("service") ServiceProvider service, Model model) {
+        // public String rendService(@RequestBody Map<String, String> params, Model
+        // model) {
+        // public String renderService(@ModelAttribute("service") ServiceProvider
+        // service, Model model) {
+        System.out.println(service.getId());
+        // ServiceProvider service = serviceProviderRepo.findById(id);
+        // model.addAttribute("service", service);
+        return "service";
     }
 
     @PostMapping("/register")
