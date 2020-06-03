5e4ad3ff13c710000c20690f8f42a0bef880846e
vaibhavhirani <vaibhavhirani@gmail.com>
2020-06-03 22:49:05 +0530

    user profile somewhat done

--- src/main/resources/static/js/userProfile.js
+++ src/main/resources/static/js/userProfile.js
@@ -1,20 +1,41 @@
 
 $(document).ready(function() {
-    $(".edit-profile-container").show()
+
+    $(".view-profile-container").show()
     $(".your-orders-container").hide()
-})
 
-
-$(document).ready(function() {
-    $(".edit-profile-btn").click(function() {
-        $(".edit-profile-container").show()
+    $(".view-profile-btn").click(function() {
+        $(".view-profile-container").show()
         $(".your-orders-container").hide()
     })
-});
 
-$(document).ready(function() {
     $(".your-orders-btn").click(function() {
-        $(".edit-profile-container").hide()
+        $(".view-profile-container").hide()
          $(".your-orders-container").show()
     })
+
+    $(".edit-button-view-profile").click(function() {
+        event.preventDefault();
+        $(".edit-button-view-profile").hide();
+        $(".save-button-view-profile").show();
+        $(".close-button-view-profile").show();
+        $(".editableContent").attr("contenteditable", "true");
+    })
+
+    $(".save-button-view-profile").click(function() {
+        event.preventDefault(); 
+        $(".edit-button-view-profile").show();
+        $(".save-button-view-profile").hide();
+        $(".close-button-view-profile").hide();
+        $(".editableContent").attr("contenteditable", "false");
+    })
+
+    $(".close-button-view-profile").click(function() {
+        event.preventDefault();
+        $(".edit-button-view-profile").show();
+        $(".save-button-view-profile").hide();
+        $(".close-button-view-profile").hide();
+        $(".editableContent").attr("contenteditable", "false");
+    })
+
 });
