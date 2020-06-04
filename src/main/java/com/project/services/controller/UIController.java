8dedf27d7c9013ae9f46c98f02f12e46d86cc56d
Akshat Ahuja <akshat.ahuja@sap.com>
2020-06-05 04:51:34 +0530

    fixed error

--- src/main/java/com/project/services/controller/UIController.java
+++ src/main/java/com/project/services/controller/UIController.java
@@ -1,6 +1,6 @@
 package com.project.services.controller;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+// import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.project.services.model.Location;
 import com.project.services.model.Service;
 import com.project.services.model.ServiceProvider;
@@ -15,25 +15,25 @@ import com.project.services.service.userDetailsService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+// import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 // import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+// import org.springframework.http.MediaType;
 
 // Session Management
-import javax.jws.soap.SOAPBinding;
+// import javax.jws.soap.SOAPBinding;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+// import java.util.Map;
+// import java.util.Optional;
 
 @Controller
 public class UIController {
