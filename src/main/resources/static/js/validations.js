06e1cdc1dcdd01f40e8b8536a7f821648b60c902
vaibhavhirani <vaibhavhirani@gmail.com>
2020-06-07 19:52:07 +0530

    Overriding default valiadation provided by browser

--- src/main/resources/static/js/validations.js
+++ src/main/resources/static/js/validations.js
@@ -9,35 +9,31 @@ function validateEmail(formDOMName, inputDOMName) {
   var email = document.forms[formDOMName][inputDOMName];
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-  if (email.value == "") {
-    event.preventDefault();
-    if(email.classList.contains('is-valid')) {
-      email.classList.remove('is-valid');
-    }
-    email.classList.add('is-invalid');
-    feedback = document.getElementById('invalid-email');
-    feedback.innerHTML = "Please provide an email"   
-  }
+  email.classList.remove('is-valid');
+  email.classList.remove('is-invalid');
   
   if(email.value.match(mailformat)) {
-    
-    if(email.classList.contains('is-invalid')) {
-      email.classList.remove('is-invalid');
-    }
     email.classList.add('is-valid');
     feedback = document.getElementById('valid-email');
     feedback.innerHTML = "Legit" 
+  }
+
+  else{
+    event.preventDefault();
+    email.classList.add('is-invalid');
+    feedback = document.getElementById('invalid-email');
+    feedback.innerHTML = "Please provide an email"   
   }
 }
 
 function validatePassword(formDOMName, inputDOMName) {
   
   var password = document.forms[formDOMName][inputDOMName];
+  password.classList.remove('is-valid');
+  password.classList.remove('is-invalid');
+  
   if (password.value == "") {
     event.preventDefault();
-    if(password.classList.contains('is-valid')) {
-      password.classList.remove('is-valid');
-    }
     password.classList.add('is-invalid');
     feedback = document.getElementById('invalid-password');
     feedback.innerHTML = "Please provide a password"
@@ -45,9 +41,6 @@ function validatePassword(formDOMName, inputDOMName) {
   }
 
   else {
-    if(password.classList.contains('is-invalid')) {
-      password.classList.remove('is-invalid');
-    }
     password.classList.add('is-valid');
     feedback = document.getElementById('invalid-password');
     feedback.innerHTML = "Please provide a password"
