44822d662d7d2fda25ae5dffd7b4043255be1480
vaibhavhirani <vaibhavhirani@gmail.com>
2020-06-07 19:35:31 +0530

    Email and Password validations done for Login form

--- src/main/resources/static/js/validations.js
+++ src/main/resources/static/js/validations.js
@@ -1,24 +1,71 @@
-function validateForm() {
-    var x = document.forms["login"]["email"].value;
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+function validateLoginForm() {
+  validateEmail("login", "email", "asdas");
+  validatePassword("login", "password", "asfas");
+}
 
-  if (x == "" ) {
-    alert("Email is one of the essentials");
+
+function validateEmail(formDOMName, inputDOMName) {
+
+  var email = document.forms[formDOMName][inputDOMName];
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (email.value == "") {
+    event.preventDefault();
+    if(email.classList.contains('is-valid')) {
+      email.classList.remove('is-valid');
+    }
+    email.classList.add('is-invalid');
+    feedback = document.getElementById('invalid-email');
+    feedback.innerHTML = "Please provide an email"   
+  }
+  
+  if(email.value.match(mailformat)) {
+    
+    if(email.classList.contains('is-invalid')) {
+      email.classList.remove('is-invalid');
+    }
+    email.classList.add('is-valid');
+    feedback = document.getElementById('valid-email');
+    feedback.innerHTML = "Legit" 
+  }
+}
+
+function validatePassword(formDOMName, inputDOMName) {
+  
+  var password = document.forms[formDOMName][inputDOMName];
+  if (password.value == "") {
+    event.preventDefault();
+    if(password.classList.contains('is-valid')) {
+      password.classList.remove('is-valid');
+    }
+    password.classList.add('is-invalid');
+    feedback = document.getElementById('invalid-password');
+    feedback.innerHTML = "Please provide a password"
     return false;
   }
 
-  
-if(x.match(mailformat))
-{
-console.log("hello");
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
+  else {
+    if(password.classList.contains('is-invalid')) {
+      password.classList.remove('is-invalid');
+    }
+    password.classList.add('is-valid');
+    feedback = document.getElementById('invalid-password');
+    feedback.innerHTML = "Please provide a password"
+  }
 
-console.log("hello");
-return false;
 }
-    
+
+//verify
+
+function verifyEmail(formDOMName, inputDOMName) {
+
+  var email = document.forms[formDOMName][inputDOMName];
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (email.value == "") {
+    event.preventDefault();
+    email.classList.add('is-invalid');
+    feedback = document.getElementById('invalid-email');
+    feedback.innerHTML = "Please provide an email"   
+  }  
 }
